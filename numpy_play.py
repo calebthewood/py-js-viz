b2616adbf23e4c13ab37291d13b29f993dc381d9
Caleb Wood <calebw787@gmail.com>
2023-10-26 16:28:46 -0700

    start pandas section

--- numpy_play.py
+++ numpy_play.py
@@ -24,3 +24,15 @@ pi = np.pi
 a = np.array([pi, pi/2, pi/4, pi/6])
 degs = np.degrees(a)
 sin_a = np.sin(a)
+rounded = np.round(sin_a, 7)
+a = np.arange(8).reshape(2,4)
+c_sum = np.cumsum(a, axis=1) # cum sum for each arr
+c_sum_flat = np.cumsum(a) # flattens arrs by default
+
+def moving_average(a, n=3):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+a = np.arange(10)
+moving_average(a, 4)
---
+++ pandas_play.py
@@ -0,0 +1,5 @@
+# Pandas is optimized for row-columnar data. tables nad csv's into chart.
+
+import pandas as pd
+
+df = pd.read_json('/data/nobel_winners.json')
